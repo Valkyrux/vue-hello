f72ec0a77d6db0a2cefeaba1b9e22690e6bbc09d
Valkyrux <philadephos.granata@gmail.com>
2021-12-15 15:40:31 +0100

    ok

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const root = new Vue ({
     el: "#root",
     data: {
         message: "Vue Hello",
+        helloimg: "https://images.unsplash.com/photo-1639150171299-c929a8486106?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
     }
 
 });
